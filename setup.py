a486b69cb3fc37ec4745de7b28ae732a7c5e78fa
Gordana Ispirova <166062997+gispirova@users.noreply.github.com>
2024-06-03 17:19:51 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='NetMedPy',
+    version='0.1.0',
+    author='Andrés Aldana, Michael Sebek, Gordana Ispirova, Rodrigo Dorantes-Gilardi, Giulia Menichetti',
+    author_email='giulia.menichetti@channing.harvard.edu',
+    description='NetMedPy evaluates network localization (statistical analysis of the largest connected component/subgraph or LCC), calculates proximity and separation between biological entities, and conducts screenings involving a large number of diseases and drug targets. NetMedPy extends the traditional Network Medicine analyses by providing four default network metrics (shortest paths, random walk, biased random walk, communicability) and four null models (perfect degree match, degree logarithmic binning, strength logarithmic binning, uniform). The user is allowed to introduce custom metrics and null models.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/menicgiulia/NetMedPy',
+    packages=find_packages(),
+    install_requires=[
+            'networkx==3.3',
+            'ray==2.20.0',
+            'numpy==1.26.4',
+            'pandas==2.2.2'
+        ],
+    extras_require={
+        'interactive': ['seaborn==0.13.2', 'matplotlib==3.9.0', 'jupyter'],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.7, <3.12',  # Minimum and maximum Python version
+)
