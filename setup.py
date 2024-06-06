adbab19e2a4b2dc58bb51760d8cf0e723a590092
Gordana Ispirova <166062997+gispirova@users.noreply.github.com>
2024-06-06 17:09:21 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    version='0.1.42',
+    version='0.1.162',
     name='NetMedPy',
     author='Andrés Aldana, Michael Sebek, Gordana Ispirova, Rodrigo Dorantes-Gilardi, Giulia Menichetti',
     author_email='giulia.menichetti@channing.harvard.edu',
@@ -16,24 +16,18 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3 :: Only',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
-    python_requires='>=3.7, <3.12',
+    python_requires='>=3.8, <3.12',
     install_requires=[
-        'networkx',
-        'ray>=2.20.0,<3.0.0',  # A
-        'numpy>=1.21.0,<2.0.0',
-        'pandas>=1.3.0,<3.0.0',
-        'seaborn>=0.11.0,<0.14.0',
-        'matplotlib>=3.0.0,<4.0.0',
-        'scipy>=1.0.1,<2.0.0'
-
+        "numpy",
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "scipy",
+        "networkx",
+        "ray"
     ],
     extras_require={
         'interactive': ['jupyter'],
